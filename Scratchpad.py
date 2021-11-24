15be735fd7452718eca59b4e6985e75a373412ef
ostamo1 <ostamo1@gmail.com>
2021-11-23 22:27:20 -0500

    Learned about variable types and tuples today

---
+++ Scratchpad.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+#11-23-2021 Learning about Variable types in python being mutable or immutable
+#also learning about precision and fractions and Booleans and floats and ints
+
+#tuples
+t=()
+type(t)
+one_element_tuple = (42,)
+three_element_tuple = (1,2,3)
+a,b,c = 4,5,6
+print(a)
+print(b)
+print(c)
+print(one_element_tuple)
+print(three_element_tuple)
+print(3 in three_element_tuple)
